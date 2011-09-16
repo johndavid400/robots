bf975c288cc78fa5105b9a3bc7892b3421eb9343
JD_home <johndavid400@yahoo.com>
2011-09-16 02:06:35 -0500

    [1]

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Robots::Application.config.secret_token = '9780eb5c69ef88aceaac2dbb09005e4198f6bcb58efeb094b63513c7d24df75ac0464c55ed46967d660950c41b96a4592593f084b94b4c5f730412bace1da13d'
