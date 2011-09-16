81e0138d7ecc2569995b8a7bb387f3fbd3e61ee4
JD_home <johndavid400@yahoo.com>
2011-09-16 02:15:33 -0500

    2nd commit - working spree app 0.60.1

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,12 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+(function($){
+  $(document).ready(function(){
+
+    // Remove an item from the cart by setting its quantity to zero and posting the update form
+    $('form#updatecart a.delete').show().live('click', function(e){
+      $(this).parents('tr').find('input.line_item_quantity').val(0);
+      $(this).parents('form').submit();
+      e.preventDefault();
+    });
+
+  });
+})(jQuery);
